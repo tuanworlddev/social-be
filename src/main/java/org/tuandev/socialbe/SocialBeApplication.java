ff2e3d5e776cac02e0fd812656e7e94bc7280d76
tuanworlddev <nanhtuan.it@gmail.com>
2024-11-23 05:38:08 +0700

    setup

---
+++ src/main/java/org/tuandev/socialbe/SocialBeApplication.java
@@ -0,0 +1,13 @@
+package org.tuandev.socialbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tuandev/socialbe/SocialBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tuandev.socialbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
